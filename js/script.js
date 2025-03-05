bbc7f94f930e2ad5e98ac787ccaf14f004943371
Hector Belchior <139378121+negohector@users.noreply.github.com>
2025-03-04 22:14:05 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function fuja() {
+    var botaoNao = document.getElementById("nao")
+
+    var larguraJanela = window.innerWidth;
+    var alturaJanela = window.innerHeight;
+
+    var maxX = larguraJanela - botaoNao.offsetWidth;
+    var maxY = alturaJanela - botaoNao.offsetHeight;
+
+    var aleatorioX = Math.floor(Math.random() * maxX);
+    var aleatorioY = Math.floor(Math.random() * maxY);
+
+    botaoNao.style.left = aleatorioX + "px";
+    botaoNao.style.top = aleatorioY + "px";
+    
+}
+function parabens() {
+    alert("Parabéns! Você fez uma sábia escolha!");
+    open("./aceitou.html");
+}
